aebda6b648a5936f3e532bc531b1165bc49ff866
Simon Buxton <simon@buxton.geek.nz>
2020-07-03 11:14:35 +0100

    add a couple more simple endpoints to app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py - a minimal flask api using flask_restful
 from flask import Flask
 from flask_restful import Resource, Api
+from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,7 +10,18 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+class Ping(Resource):
+    def get(self):
+        return {'ping': 'pong'}
+
+class Healthcheck(Resource):
+    def get(self):
+        the_time = str(datetime.now())
+        return {'servertime': the_time}
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(Ping, '/ping')
+api.add_resource(Healthcheck, '/healthcheck')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
